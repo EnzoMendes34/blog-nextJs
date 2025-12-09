81d55e6a45647cbdcfbaaf882f09ed4c9ee9aebc
EnzoMendes34 <enzomendes150124@gmail.com>
2025-12-09 10:18:08 -0300

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,19 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "The Blog",
+  description: "Generated during a Next.Js course",
+};
+
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
+  return (
+    <html lang="pt-BR">
+      <body>{children}</body>
+    </html>
+  );
+}
