22a580ef8ade2a1194de9a2a45351bc40d263276
EnzoMendes34 <enzomendes150124@gmail.com>
2026-01-06 06:24:15 -0300

    Creating components for Admin area

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,25 +1,11 @@
-import { Button } from "@/components/Button";
-import { TagIcon } from "lucide-react";
+import { ManagePostForm } from "@/components/admin/ManagePostForm";
 
 export const dynamic = "force-dynamic";
 
 export default async function AdminPostNewPage() {
   return (
     <>
-      <div className='py-16 flex gap-4 items-center'>
-        <Button size='sm' variant='default'>
-          <TagIcon />
-          Confirmar
-        </Button>
-        <Button size='md' variant='danger'>
-          <TagIcon />
-          Cancelar
-        </Button>
-        <Button size='lg' variant='ghost'>
-          <TagIcon />
-          Beleza
-        </Button>
-      </div>
+      <ManagePostForm />
     </>
   );
 }
